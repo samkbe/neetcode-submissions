64f5ee4da9f64d12c2bc8e6cfab40c5061e9d06c
Sam B <96403295+samkbe@users.noreply.github.com>
2026-06-28 16:00:27 -0700

    Add: longest-substring-without-duplicates - submission-9

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-9.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        if (s.length < 2) return s.length;
+        let maxLength = 0;
+        let l = 0;
+        let r = 1;
+
+        const windowMap = {[s[l]]: true }
+
+        while (r < s.length) {
+            if (windowMap[s[r]]) {
+                while ( l < r && windowMap[s[r]]) {
+                    delete windowMap[s[l]];
+                    l++;
+                }
+                windowMap[s[r]] = true;
+            } else {
+                windowMap[s[r]] = true;
+            }
+            maxLength = Math.max(maxLength, (r - l + 1));
+            r++;
+        }
+        return maxLength;
+    }
+}
