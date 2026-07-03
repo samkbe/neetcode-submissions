c02d8b2fef15e5b47849483bfaf54e401e7331da
Sam B <96403295+samkbe@users.noreply.github.com>
2026-07-02 20:15:20 -0700

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0;
+        let r = nums.length - 1;
+
+        while (l <= r) {
+            const mid = Math.floor((l + r) / 2);
+
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else if (nums[mid] > target) {
+                r = mid - 1;                
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+
+//            |
+//      |
+//[-1,0,2,4,6,8]
