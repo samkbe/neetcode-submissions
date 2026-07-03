68dcb178b0e6f2da839d373412008164a2a579dd
Sam B <96403295+samkbe@users.noreply.github.com>
2026-07-03 15:46:16 -0700

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        piles.sort((a, b) => a - b);
+        let l = 1;
+        let r = piles[piles.length - 1];
+        let leastNum = r;
+
+        while (l <= r) {
+            const midNum = Math.floor((l + r) / 2);
+            let accumulator = 0;
+            for (let pile of piles) {
+                accumulator += Math.ceil(pile / midNum);
+                if (accumulator > h) {
+                    break;
+                }
+            }
+            if (accumulator <= h) {
+                leastNum = midNum;
+                r = midNum - 1;
+            } else {
+                l = midNum + 1;
+            }
+        }
+        return leastNum;
+    }
+}
