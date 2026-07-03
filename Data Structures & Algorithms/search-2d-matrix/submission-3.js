4a997ce2d0ff8a51a361d6c91ddd96e40993c1a8
Sam B <96403295+samkbe@users.noreply.github.com>
2026-07-02 20:50:11 -0700

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        const h = matrix.length;
+        const w = matrix[0].length;
+
+        let l = 0;
+        let r = (w * h) - 1;
+
+        while (l <= r) {
+            let mid = Math.floor((r + l) / 2);
+            let midValue = matrix[Math.floor(mid / w)][mid % w];
+
+            if (midValue < target) {
+                l = mid + 1;
+            } else if (midValue > target) {
+                r = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
