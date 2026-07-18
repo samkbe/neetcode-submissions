3797494a8fcb277c66ee008d3c2c3935d98fe7d0
Sam B <96403295+samkbe@users.noreply.github.com>
2026-07-18 11:04:43 -0700

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.js
@@ -0,0 +1,41 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        const oldToNew = new Map();
+
+        function dfs(node) {
+            if (!node) return
+            if (oldToNew.has(node)) return oldToNew.get(node);
+
+            const newNode = new Node(node.val);
+            oldToNew.set(node, newNode);
+
+            for (let neighbor of node.neighbors ) {
+                newNode.neighbors.push(dfs(neighbor));
+            }
+
+            return newNode;
+        }
+
+        const output = dfs(node);
+        console.log(oldToNew.size)
+        return !oldToNew.size ? null : output;
+
+    }
+}
+
+// 1
+//[[2],[1,3],[2]]
