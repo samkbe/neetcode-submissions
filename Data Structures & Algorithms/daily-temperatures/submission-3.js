e28bb002c41324252152d11f315253a098952a22
Sam B <96403295+samkbe@users.noreply.github.com>
2026-07-20 20:20:51 -0700

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const stack = [];
+        const output = [];
+
+        for (let i = 0; i < temperatures.length; i++) {
+            while (stack.length && temperatures[i] > stack[stack.length - 1][0]) {
+                const [_, prevIndex] = stack.pop();
+                output[prevIndex] = i - prevIndex;
+            }
+            stack.push([temperatures[i], i]);
+        }
+        while (stack.length) {
+            const [_, index] = stack.pop();
+            output[index] = 0;
+        }
+        return output;
+    }
+}
