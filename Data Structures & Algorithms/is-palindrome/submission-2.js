113bfd2c318db0feb578d5d320ab7ab942773f9e
Sam B <96403295+samkbe@users.noreply.github.com>
2026-07-24 16:28:01 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,70 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const chars = {
+            "a": true,
+            "b": true,
+            "c": true,
+            "d": true,
+            "e": true,
+            "f": true,
+            "g": true,
+            "h": true,
+            "i": true,
+            "j": true,
+            "k": true,
+            "l": true,
+            "m": true,
+            "n": true,
+            "o": true,
+            "p": true,
+            "q": true,
+            "r": true,
+            "s": true,
+            "t": true,
+            "u": true,
+            "v": true,
+            "w": true,
+            "x": true,
+            "y": true,
+            "z": true,
+            "0": true,
+            "1": true,
+            "2": true,
+            "3": true,
+            "4": true,
+            "5": true,
+            "6": true,
+            "7": true,
+            "8": true,
+            "9": true,
+        }
+        let l = 0;
+        let r = s.length - 1;
+
+        s = s.toLowerCase();
+
+        while (l < r) {
+            if (!chars.hasOwnProperty(s[l])) {
+                l++;
+                console.log(s[l]);
+                continue;
+            }
+            if (!chars.hasOwnProperty(s[r])) {
+                r--;
+                console.log(s[r]);
+                continue;
+            }
+            console.log('left:', s[l], l, ' right: ', s[r], r);
+            if (s[l] !== s[r]) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
