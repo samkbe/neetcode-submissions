f18f57ed1da9f9e9b2b71e897a7d043567a1b6af
Sam B <96403295+samkbe@users.noreply.github.com>
2026-08-04 16:30:02 -0700

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        let rob1 = 0;
+        let rob2 = 0;
+
+        for (let n of nums) {
+            let temp = Math.max(n + rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+
+        return rob2;
+    }
+}
