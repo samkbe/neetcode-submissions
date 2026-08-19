4bf8aef9902e9ad39c555bb63600c7fb4577cdbc
Sam B <96403295+samkbe@users.noreply.github.com>
2026-08-19 07:35:57 -0700

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeight(stones) {
+        const pq = new MaxPriorityQueue();
+
+        for (let stone of stones) {
+            pq.enqueue(stone);
+        }
+
+        while (pq.size() > 1) {
+            let first = pq.dequeue();
+            let second = pq.dequeue();
+            pq.enqueue(Math.abs(first - second));
+        }
+
+        return pq.size() > 0 ? pq.dequeue() : 0;
+    }
+}
