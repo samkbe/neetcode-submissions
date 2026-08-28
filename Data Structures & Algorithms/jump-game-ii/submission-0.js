d8355ef4982f3d8bdde51b5cb6fdd12302ed79d1
Sam B <96403295+samkbe@users.noreply.github.com>
2026-08-28 06:31:30 -0700

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    jump(nums) {
+        let res = 0;
+        let l = 0;
+        let r = 0;
+
+        while (r < nums.length - 1) {
+            let furthest = 0;
+            for (let i = l; i <= r; i++) {
+                furthest = Math.max(furthest, i + nums[i]);
+            }
+            l = r + 1;
+            r = furthest;
+            res++;
+        }
+
+        return res;
+    }
+}
