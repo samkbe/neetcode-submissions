6a0d26970102e6d984c010c850b5ced7dfefed34
Sam B <96403295+samkbe@users.noreply.github.com>
2026-09-12 12:51:04 -0700

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        intervals.sort((a, b) => a[0] - b[0]);
+        const output = [];
+
+        let curr = intervals[0];
+
+        for (let i = 1; i < intervals.length; i++) {
+            const next = intervals[i];
+
+            if (next[0] <= curr[1]) {
+                curr[1] = Math.max(curr[1], next[1]);
+            } else {
+                output.push(curr);
+                curr = next;
+            }
+        }
+        output.push(curr);
+        return output;
+    }
+}
