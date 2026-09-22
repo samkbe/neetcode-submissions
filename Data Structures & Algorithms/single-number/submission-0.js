23263d89a28bcc3a5b2a56d25b746ca04db22ad3
Sam B <96403295+samkbe@users.noreply.github.com>
2026-09-22 16:31:57 -0700

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums) {
+        let res = 0;
+        for (let num of nums) {
+            res ^= num;
+        }
+        return res;
+    }
+}
